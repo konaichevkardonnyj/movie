c7a8308c80148c99196b8e95ea48f5447abbc624
Vasya <konaichevkardonnyj@mail.ru>
2021-10-03 21:08:18 +0300

    movie

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 const numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
 //document.write(numerofFilms);
 //console.log(numerofFilms);
-const a= prompt("Один из последних фильмов?", ""),
-      b= prompt("На сколько оцените его?", ""),
-      c= prompt("Один из последних фильмов?", ""),
-      d= prompt("На сколько оцените его?", "");
 const personalMovieDB = {
     count: numerofFilms,
-    movie: {},
+    movie: [],
     actors: {},
     genres: [],
     privat: false,
     };
+const a= prompt("Один из последних фильмов?", ""),
+    b= prompt("На сколько оцените его?", ""),
+    c= prompt("Какой жанр вы любите?", ""),
+    d= prompt("Любите ли вы плакать под фильм?", "");
+personalMovieDB.movie[a] = b;
+personalMovieDB.movie[c] = d;
+console.log(personalMovieDB);
