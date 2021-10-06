0aa01608a3c253ba2fd67ecdacba4c2988965777
Vasya <konaichevkardonnyj@mail.ru>
2021-10-06 21:31:12 +0300

    movie

--- javascript.js
+++ javascript.js
@@ -1,17 +1,33 @@
-const numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
-//document.write(numerofFilms);
-//console.log(numerofFilms);
+let numerofFilms;
+start();
+remenberMyFilms();
 const personalMovieDB = {
     count: numerofFilms,
-    movie: [],
+    movie: {},
     actors: {},
     genres: [],
     privat: false,
-    };
-const a= prompt("Один из последних фильмов?", ""),
-    b= prompt("На сколько оцените его?", ""),
-    c= prompt("Какой жанр вы любите?", ""),
-    d= prompt("Любите ли вы плакать под фильм?", "");
-personalMovieDB.movie[a] = b;
-personalMovieDB.movie[c] = d;
+    }
+    let i;
+function remenberMyFilms() {
+    for (let i=0; i<2; i++) {
+         const a = prompt("Один из последних фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+        personalMovieDB.movie[a] = b;
+        console.log('done')
+        if (a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
+        } else {
+            console.log('error')
+            i--;
+        }
+    }
+}
+
 console.log(personalMovieDB);
+function start() {
+    numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+    while
+        (numerofFilms == "" || numerofFilms == null || isNaN(numerofFilms)) {
+        numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+    }
+}
