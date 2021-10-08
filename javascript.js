f6e1245f56cfbc46fb218bc9787c2e8b66816794
konaichevkardonnyj <88146256+konaichevkardonnyj@users.noreply.github.com>
2021-10-08 13:00:53 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,33 +1,59 @@
-let numerofFilms;
-start();
-remenberMyFilms();
+const numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+//document.write(numerofFilms);
+//console.log(numerofFilms);
 const personalMovieDB = {
     count: numerofFilms,
     movie: {},
     actors: {},
     genres: [],
     privat: false,
-    }
-    let i;
-function remenberMyFilms() {
-    for (let i=0; i<2; i++) {
-         const a = prompt("Один из последних фильмов?", ""),
-          b = prompt("На сколько оцените его?", "");
-        personalMovieDB.movie[a] = b;
-        console.log('done')
-        if (a != '' && b != '' && a != null && b != null && a.length < 50 && b.length < 50) {
-        } else {
-            console.log('error')
-            i--;
-        }
-    }
-}
+    };
 
-console.log(personalMovieDB);
-function start() {
-    numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
-    while
-        (numerofFilms == "" || numerofFilms == null || isNaN(numerofFilms)) {
-        numerofFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних фильмов?", ""),
+         b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
+           personalMovieDB.movie[a] = b;
+    console.log('yes');
+    }
+  else {
+       console.log('no');
+       i--;
     }
 }
+//console.log(personalMovieDB);
+
+//if (personalMovieDB.count<10) {
+//    alert('Просмотрено мало фильмов')
+//} else if (personalMovieDB.count<=30) {
+//    alert('Стандартный телезритель');
+//} else if (personalMovieDB.count>30) {
+//    alert('Вы киноман')
+//} else {
+//    alert('Произошла ошибка')
+//}
+//let i = 0;
+do {
+      const a = prompt("Один из последних фильмов?", ""),
+         b = prompt("На сколько оцените его?", "");
+      i++;
+      if (a != '' && b != '' && a != null && a.length<50) {
+          console.log('yes');
+          personalMovieDB.movie[a] = b;
+      } else {
+          i--;
+          console.log('no');
+      }
+  }
+    while (i < 2);
+console.log(personalMovieDB);
+
+function show(text, a, b) {
+    console.log(text)
+}
+show('WORLD');
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc (4, 3));
---
+++ script.js
@@ -0,0 +1,60 @@
+let numerofFilms;
+const personalMovieDB = {
+    count: numerofFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+ShowMyDB(personalMovieDB.privat);
+Start();
+writeYourGenres();
+PersonalLevel();
+RememberMyFilms();
+function ShowMyDB(privat) {
+    if (!privat){
+        console.log(personalMovieDB)
+           }
+}
+
+function Start() {
+    numerofFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+    while (numerofFilms == '' || numerofFilms == null || isNaN(numerofFilms)) {
+        numerofFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+        console.log('no');
+    }
+    console.log('yes');
+    personalMovieDB.count = numerofFilms;
+}
+
+    function writeYourGenres() {
+        for (let i = 1; i <= 3; i++) {
+            const genres = prompt(`Ваш любимы жанр по номерам ${i}`);
+            personalMovieDB.genres [i-1] = genres;
+        }
+    }
+function PersonalLevel() {
+    if (personalMovieDB.count<10) {
+        alert('Просмотрено мало фильмов')
+    } else if (personalMovieDB.count<=30) {
+        alert('Стандартный телезритель');
+    } else if (personalMovieDB.count>30) {
+        alert('Вы киноман')
+    } else {
+        alert('Произошла ошибка')
+    }
+}
+function RememberMyFilms() {
+   // for (let i = 0; i < 1; i++) {
+            let a = prompt("Один из последних фильмов?", ""),
+                b = prompt("На сколько оцените его?", "");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ) {
+            personalMovieDB.movie[a] = b;
+        }
+        else {
+            i--;
+        }
+ //   }
+}
+
+//console.log(personalMovieDB);
